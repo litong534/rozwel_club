b973482b2482ea32bbf391778810a3b833de887d
litong534 <litong534@sina.com>
2017-12-28 15:29:50 +0800

    upload function will return url of the images uploaded, instead of base64 to prevent long time loading in browser

--- server/api/uploads.js
+++ server/api/uploads.js
@@ -10,12 +10,13 @@ router.route('/').post(function (req, res) {
   // form.uploadDir = 'E:\\tmp\\';
   form.parse(req, function (err, fields, files) {
     var tmp_path = files.files.path;
-    var target_path = './uploads/images/' + files.files.name;
+    var target_path = '/var/www/uploads/images/' + files.files.name;
     fs.rename(tmp_path, target_path, function (err) {
       if (err) throw err;
-      var bData = fs.readFileSync(target_path);
-      var base64Str = bData.toString('base64');
-      var datauri = 'data:image/png;base64,' + base64Str;
+      // var bData = fs.readFileSync(target_path);
+      // var base64Str = bData.toString('base64');
+      // var datauri = 'data:image/png;base64,' + base64Str;
+      let datauri = 'http://47.95.250.143/uploads/images/' + files.files.name;
       // 删除临时文件夹文件, 
       fs.unlink(tmp_path, function () {
         if (err) throw err;
